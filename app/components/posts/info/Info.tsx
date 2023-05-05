0f6ddf678f2c8eb6c05574be1a659d2839d12c12
Kamil Jozwik <kamiljozwikpl@gmail.com>
2023-05-05 10:29:46 +0200

    post: obiekty JS - wstep

---
+++ app/components/posts/info/Info.tsx
@@ -0,0 +1,31 @@
+import { PropsWithChildren, CSSProperties } from "react";
+import styles from "./infox.module.scss";
+/* Not working with MDX - why? */
+
+type Props = {
+  type?: "info";
+  title?: string;
+};
+
+const style: { [key: string]: CSSProperties } = {
+  root: {
+    borderStyle: "solid",
+    borderColor: "var(--color-links)",
+    borderWidth: "4px 1px 1px",
+    padding: "8px 16px",
+    marginBottom: "12px",
+  },
+};
+
+export const Info = ({
+  children,
+  title,
+  type = "info",
+}: PropsWithChildren<Props>) => {
+  return (
+    <div className={styles.root} style={style.root}>
+      {title ? <h4>{title}</h4> : null}
+      {children}
+    </div>
+  );
+};

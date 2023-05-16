fdf5b79ad055d8c90a5dfb6908f1448d346d3fba
Kamil Jozwik <kamiljozwikpl@gmail.com>
2023-05-16 14:45:37 +0200

    add new posts

--- app/components/posts/info/Info.tsx
+++ app/components/posts/info/Info.tsx
@@ -3,18 +3,22 @@ import styles from "./infox.module.scss";
 /* TODO: Not working with MDX - why? */
 
 type Props = {
-  type?: "info";
+  type?: "info" | "warning" | "danger";
   title?: string;
 };
 
-const style: { [key: string]: CSSProperties } = {
-  root: {
-    borderStyle: "solid",
-    borderColor: "var(--color-links)",
-    borderWidth: "4px 1px 1px",
-    padding: "8px 16px",
-    marginBottom: "24px",
-  },
+const colorsMapper = (type: Props["type"]) => {
+  switch (type) {
+    case "info":
+      return "var(--color-links)";
+    case "warning":
+      return "var(--color-headers)";
+    case "danger":
+      return "var(--color-primary)";
+
+    default:
+      return "white";
+  }
 };
 
 export const Info = ({
@@ -22,6 +26,16 @@ export const Info = ({
   title,
   type = "info",
 }: PropsWithChildren<Props>) => {
+  const style: { [key: string]: CSSProperties } = {
+    root: {
+      borderStyle: "solid",
+      borderColor: colorsMapper(type),
+      borderWidth: "4px 1px 1px",
+      padding: "8px 16px",
+      marginBottom: "24px",
+    },
+  };
+
   return (
     <div className={styles.root} style={style.root}>
       {title ? <h4>{title}</h4> : null}

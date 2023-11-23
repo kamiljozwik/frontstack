70898ad0068470bdf192cf33c9e3743974868376
Kamil Jozwik <kamiljozwikpl@gmail.com>
2023-11-23 14:52:52 +0100

    add some shorts from my bookmarks

--- app/shorts/page.tsx
+++ app/shorts/page.tsx
@@ -1,6 +1,7 @@
 import Content from "./content.mdx";
 import { PageHeader } from "../components/headers/Headers";
 import style from "./shorts.module.scss";
+import { ExternalLink } from "../components/externalLink/ExternalLink";
 
 export const metadata = {
   title: "Frontend - użyteczne informacje",
@@ -21,6 +22,13 @@ const Shorts = () => {
       <PageHeader desc="Krótkie informacje / sztuczki / rozwiązania, które mogą Ci się przydać w codziennej pracy 😉">
         Shorts
       </PageHeader>
+      <div className="p-3 bg-gray-400 rounded-lg">
+        Masz pomysł na nietypowego i pomocnego Shorta? Daj mi o tym znać na{" "}
+        <ExternalLink href="https://discord.gg/Jg9aSAuxpP">
+          Discordzie
+        </ExternalLink>
+        🔗
+      </div>
       <Content />
     </main>
   );
